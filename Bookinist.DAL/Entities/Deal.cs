ad63a165d38c554a36843ea3b0031819171a6449
Darya Akhromenko <otulissa2012@yandex.ru>
2022-05-26 20:22:34 +0700

    Структура базы данных

---
+++ Bookinist.DAL/Entities/Deal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bookinist.DAL.Entities.Base;
+
+namespace Bookinist.DAL.Entities
+{
+    public class Deal : Entity
+    {
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+
+        public virtual Seller Seller { get; set; }
+
+        public virtual Buyer Buyer { get; set; }
+    }
+}
